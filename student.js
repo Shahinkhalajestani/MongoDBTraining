7f5839c1eecc46be087f52659ac002ddc86eb658
sh.khalajestanii <NiH@sh123>
2022-01-31 16:44:31 +0330

    added student js

---
+++ student.js
@@ -0,0 +1,11 @@
+student= {
+    "firstName" : "shahin" ,
+    "lastName"  : "khalajestani" ,
+    "email" : "shahin.Yahoo@mail.com" ,
+    "gneder" : "MALE" ,
+    "country" : "IR" ,
+    "isStudentActive" : false ,
+    "favoriteSubjects" : ["mathematics",
+      "SE","JD"] , 
+    "totalTimeSpendInBookds" : 0.00
+}
