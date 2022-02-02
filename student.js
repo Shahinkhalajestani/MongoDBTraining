da31cc60cd1be8a22dc5af83a8499fab8e3f6287
sh.khalajestanii <NiH@sh123>
2022-02-02 13:04:08 +0330

    Students with multiple properties

--- student.js
+++ student.js
@@ -1,11 +1,58 @@
-student= {
-    "firstName" : "shahin" ,
-    "lastName"  : "khalajestani" ,
-    "email" : "shahin.Yahoo@mail.com" ,
-    "gneder" : "MALE" ,
-    "country" : "IR" ,
-    "isStudentActive" : false ,
-    "favoriteSubjects" : ["mathematics",
-      "SE","JD"] , 
-    "totalTimeSpendInBookds" : 0.00
-}
+student = {
+  "firstName": "shahin",
+  "lastName": "khalajestani",
+  "email": "shahin.Yahoo@mail.com",
+  "gneder": "MALE",
+  "country": "IR",
+  "isStudentActive": false,
+  "favoriteSubjects": ["mathematics",
+    "SE", "JD"],
+  "totalTimeSpendInBookds": 0.00
+};
+
+students = [
+  {
+    "firstName": "abbbas",
+    "lastName": "khalajestani",
+    "email": "shahin.Yahoo@mail.com",
+    "gneder": "MALE",
+    "country": "IR",
+    "isStudentActive": false,
+    "favoriteSubjects": ["asdf",
+      "SE", "JD"],
+    "totalTimeSpendInBookds": 0.00
+  },
+  {
+    "firstName": "shahla",
+    "lastName": "khalajestani",
+    "email": "shahin.Yahoo@mail.com",
+    "gneder": "MALE",
+    "country": "IR",
+    "isStudentActive": true,
+    "favoriteSubjects": ["poetary",
+      "SE", "JD"],
+    "totalTimeSpendInBookds": 0.00
+  },
+  {
+    "firstName": "shahin",
+    "lastName": "namli",
+    "email": "shahin.Yahoo@mail.com",
+    "gneder": "MALE",
+    "country": "IR",
+    "isStudentActive": false,
+    "favoriteSubjects": ["VVV",
+      "SE", "JD"],
+    "totalTimeSpendInBookds": 0.00
+  },
+  {
+    "firstName": "jahan",
+    "lastName": "jahangiri",
+    "email": "shahin.Yahoo@mail.com",
+    "gneder": "MALE",
+    "country": "IR",
+    "isStudentActive": false,
+    "favoriteSubjects": ["arithmatics",
+      "SE", "JD"],
+    "totalTimeSpendInBookds": 0.00
+  }
+]
